a8db8e3df04d1d26581e7dcdeed31714127689b1
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-14 15:07:38 -0500

    a

--- Javascript/upgradeTypes.js
+++ Javascript/upgradeTypes.js
@@ -2,6 +2,8 @@ import { stats } from "./engine.js";
 // add to click amount 
 export const upgradeTypes = {
      ADDITIVE:  (upgrade) => {
+        const amountPerClickDisplay = document.getElementById("amountPerClick");
         stats.amountPerClick += upgrade.gives;
+        amountPerClickDisplay.innerText += stats.amountPerClick
     },
 }
